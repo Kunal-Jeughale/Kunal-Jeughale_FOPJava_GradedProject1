def0de9268b31e8dec42561f30b30c3b366e6ff0
Kunal-Jeughale <111232049+Kunal-Jeughale@users.noreply.github.com>
2022-10-08 23:55:29 +0530

    Add files via upload

---
+++ Graded Coding Assignment 1 BG9/src/com/gl/model/SuperDepartment.java
@@ -0,0 +1,21 @@
+package com.gl.model;
+
+public class SuperDepartment {
+
+	public String departmentName() {
+		return "Super Department";
+	}
+
+	public String getTodaysWork() {
+		return "No work as of Now";
+	}
+
+	public String getWorkDeadline() {
+		return "Nil";
+	}
+
+	public String isTodayAHoliday() {
+		return "Today is not a Holiday";
+	}
+
+}
